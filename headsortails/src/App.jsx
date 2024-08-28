4305de4bba51badc15ba9473936075e40ac784b4
samettopcu88 <samet.topcu.88@icloud.com>
2024-08-29 00:25:05 +0300

    Add project files.

---
+++ headsortails/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import './App.css'
+import CoinContainer from './CoinContainer'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <CoinContainer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ headsortails/src/CoinContainer.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react'
+
+class CoinContainer extends Component {
+    static defaultProps = {
+        coins: [
+            {side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg'},
+            {side: 'tails', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg'}
+        ]
+    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    flipCoin() {
+    }
+
+    handleClick(e) {
+        this.flipCoin()
+    }
+    render() {
+        return (
+            <div>
+                <h1>Heads or Tails</h1>
+                <button onClick={this.handleClick}>Flip Coin</button>
+            </div>
+        )
+    }
+}
+
+export default CoinContainer
